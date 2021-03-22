2d6b8ac23ede577c38c388a8af02d48eae1bf186
BlueRex <65700898+RealBlueRex@users.noreply.github.com>
2021-03-23 00:24:37 +0900

    시험관련 코드 추가

--- mathflat.js
+++ mathflat.js
@@ -19,11 +19,35 @@ const mathflat = {
         if(token == null || classID == null) return "먼저 로그인을 해주세요"
         const res = Jsoup.connect('https://api-live.mathflat.com/classes/' + classID + '/students').header("Content-Type", "application/json;charset=utf-8").header("x-auth-token", token).ignoreHttpErrors(true)
         .ignoreContentType(true)
-        var status = res.method(org.jsoup.Connection.Method.POST).execute().statusCode()
+        var status = res.method(org.jsoup.Connection.Method.GET).execute().statusCode()
         if(status == 200) {
-            return JSON.parse(res.post().text());
+            return JSON.parse(res.get().text());
         } else {
-            return res.post().text();
+            return res.get().text();
+        }
+    },
+    examlist: function (studentId) {
+        //https://api-live.mathflat.com/students/I275529/assigned-pieces
+        if(token == null || classID == null) return "먼저 로그인을 해주세요"
+        const res = Jsoup.connect("https://api-live.mathflat.com/students/"+studentId+"/assigned-pieces").header("Content-Type", "application/json;charset=utf-8").header("x-auth-token", token).ignoreHttpErrors(true)
+        .ignoreContentType(true)
+        var status = res.method(org.jsoup.Connection.Method.GET).execute().statusCode()
+        if(status == 200) {
+            return JSON.parse(res.get().text());
+        } else {
+            return res.get().text();
+        }
+    },
+    examAnswer: function (examId) {
+        //https://api-live.mathflat.com/assigned-pieces/8895228/marking
+        if(token == null || classID == null) return "먼저 로그인을 해주세요"
+        const res = Jsoup.connect("https://api-live.mathflat.com/assigned-pieces/"+examId+"/marking").header("Content-Type", "application/json;charset=utf-8").header("x-auth-token", token).ignoreHttpErrors(true)
+        .ignoreContentType(true)
+        var status = res.method(org.jsoup.Connection.Method.GET).execute().statusCode()
+        if(status == 200) {
+            return JSON.parse(res.get().text());
+        } else {
+            return res.get().text();
         }
     }
 }

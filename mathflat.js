d2bed4bc08244fe586fa7dccb0da59cc8cf47b43
BlueRex <65700898+RealBlueRex@users.noreply.github.com>
2021-05-04 23:39:18 +0900

    add editAnswer

--- mathflat.js
+++ mathflat.js
@@ -12,9 +12,12 @@ module.exports = /** @class */ (function () {
         }
     }
     mathflat.prototype.studentList = function () {
-        if(this.token == null || this.classID == null) return "먼저 로그인을 해주세요";
-        const res = Jsoup.connect('https://api-live.mathflat.com/classes/' + this.classID + '/students').header("Content-Type", "application/json;charset=utf-8").header("x-auth-token", this.token).ignoreHttpErrors(true)
-        .ignoreContentType(true).method(org.jsoup.Connection.Method.GET).execute();
+        if(this.token == undefined || this.classID == undefined) return "먼저 로그인을 해주세요";
+        const res = Jsoup.connect('https://api-live.mathflat.com/classes/' + this.classID + '/students')
+            .header("Content-Type", "application/json;charset=utf-8")
+            .header("x-auth-token", this.token)
+            .ignoreHttpErrors(true)
+            .ignoreContentType(true).method(org.jsoup.Connection.Method.GET).execute();
         const status = res.statusCode();
         if(status == 200) {
             return JSON.parse(res.body());
@@ -23,10 +26,13 @@ module.exports = /** @class */ (function () {
         }
     }
     mathflat.prototype.examList = function (studentId) {
-        if(this.token == null || this.classID == null) return "먼저 로그인을 해주세요";
-        const res = Jsoup.connect("https://api-live.mathflat.com/students/"+studentId+"/assigned-pieces").header("Content-Type", "application/json;charset=utf-8").header("x-auth-token", this.token).ignoreHttpErrors(true)
-        .ignoreContentType(true).method(org.jsoup.Connection.Method.GET).execute();
-        var status = res.statusCode();
+        if(this.token == undefined || this.classID == undefined) return "먼저 로그인을 해주세요";
+        const res = Jsoup.connect("https://api-live.mathflat.com/students/"+studentId+"/assigned-pieces")
+            .header("Content-Type", "application/json;charset=utf-8")
+            .header("x-auth-token", this.token)
+            .ignoreHttpErrors(true)
+            .ignoreContentType(true).method(org.jsoup.Connection.Method.GET).execute();
+        const status = res.statusCode();
         if(status == 200) {
             return JSON.parse(res.body());
         } else {
@@ -34,15 +40,28 @@ module.exports = /** @class */ (function () {
         }
     }
     mathflat.prototype.examAnswer = function (examId) {
-        if(this.token == null || this.classID == null) return "먼저 로그인을 해주세요";
-        const res = Jsoup.connect("https://api-live.mathflat.com/assigned-pieces/"+examId+"/marking").header("Content-Type", "application/json;charset=utf-8").header("x-auth-token", this.token).ignoreHttpErrors(true)
-        .ignoreContentType(true).method(org.jsoup.Connection.Method.GET).execute();
-        var status = res.statusCode();
+        if(this.token == undefined || this.classID == undefined) return "먼저 로그인을 해주세요";
+        const res = Jsoup.connect("https://api-live.mathflat.com/assigned-pieces/"+examId+"/marking")
+            .header("Content-Type", "application/json;charset=utf-8")
+            .header("x-auth-token", this.token)
+            .ignoreHttpErrors(true)
+            .ignoreContentType(true).method(org.jsoup.Connection.Method.GET).execute();
+        const status = res.statusCode();
         if(status == 200) {
             return JSON.parse(res.body());
         } else {
             return res.body();
         }
+    }
+    mathflat.prototype.editAnswer = function (examID, params) {
+        if(this.token == undefined || this.classID == undefined) return "먼저 로그인을 해주세요";
+        if(typeof params === 'object') throw new Error("params is must json")
+        const res = Jsoup.connect("https://api-live.mathflat.com/assigned-pieces/"+examID+"/marking")
+            .header("Content-Type", "application/json;charset=utf-8")
+            .header("x-auth-token", this.token)
+            .requestBody(JSON.stringify(params))
+            .ignoreContentType(true).ignoreHttpErrors(true).method(org.jsoup.Connection.Method.GET).execute();
+        return res;
     }
     return mathflat;
 })();
